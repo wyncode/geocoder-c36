b002a27f247cdf497e708ec53b208dd8943aee24
Ernie Hsiung <ernie@whereby.us>
2020-03-19 12:09:38 -0400

    get current location

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,30 @@
 class PlacesController < ApplicationController
- 
-def index
-end
-
-end
+    def index
+      respond_to do |format|
+        format.html do
+          @coordinates = request.location.coordinates.reverse
+          @coordinates = [0.0, 0.0] if @coordinates.empty?
+        end
+        format.json do
+          @places = Place.all
+          render json:  {
+                          type: "FeatureCollection",
+                          features: @places.map do |place|
+                            {
+                              type: "Feature",
+                              geometry: {
+                                type: "Point",
+                                coordinates: [place.longitude, place.latitude]
+                              },
+                              properties: {
+                                name: place.name,
+                                id: place.id
+                              }
+                            }
+                          end
+                        }
+        end
+      end
+    end
+  end
+  
